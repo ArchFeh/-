e1e58ca9b25d6fff8a14667653070c75a4cea5f5
YuGu <archfeh@163.com>
2019-05-21 03:01:48 +0300

    Add files via upload

--- chess.cpp
+++ chess.cpp
@@ -5,54 +5,69 @@ using namespace std;
 
 class Parser{
     public:
-        friend istream& operator>>(istream& is,Parser& p)
+	Parser()
+	{
+		figure=new char[5];
+		start_pos=new char[3];
+		end_pos=new char[3];
+	}
+	Parser(Parser &P)
+	{
+		start_pos=new char[5];
+		end_pos=new char[3];
+		figure=P.figure;
+		start_pos=P.start_pos;
+		end_pos=P.end_pos;
+	}
+	char *Get_Fi(void)
+	{
+		return figure;
+	}
+	char *Get_sp(void)
+	{
+		return start_pos;
+	}
+	char *Get_ep(void)
+	{
+		return end_pos;
+	} 
+ 	friend istream& operator>>(istream& is,Parser& p)
         {
             is>>p.figure>>p.start_pos>>p.end_pos;
             return is;
         }
-    public:
-        char figure;
-        char start_pos[2];
-        char end_pos[2];
+	friend ostream& operator<<(ostream& os,Parser& p)
+	{
+		os<<p.figure<<p.start_pos<<p.end_pos;
+		return os;
+	}
+    private:
+        char *figure;
+        char *start_pos;
+        char *end_pos;
 };
 
 class Figure{
     public:
-        virtual bool move(const char &figure,const char *sp,char const *ep)=0;
+        virtual bool move(const char *figure,const char *sp,char const *ep)=0;
     protected:
         char figures[5]={'K','Q','N','B','R'};
 };
 
-class ChessChecker{
-    public:
-        friend ostream& operator <<(ostream& os,const ChessChecker& CC)
-        {
-            if(CC.capros)
-            os<<"OK";
-            else os<<"ERROR";
-            return os;
-        }
-    private:
-        bool capros;
-	Parser par;
-	Figure *figures[5];
-};
-
-
 class figure:public Figure
 {
     public:
-        bool move(const char &figure,const char *sp,const char *ep)
+        bool move(const char *figure,const char *sp,const char *ep)
         {
             if(sp[0]>='a' && sp[0]<='h' && sp[1]>='1' && sp[1]<='8' && ep[0]>='a' && ep[0]<='h'&& ep[1]>='1' && ep[1]<='8')
             {
-            if(figure==figures[0])
+            if(*figure==figures[0])
             {
                 if(((ep[0]-sp[0]<=1)&&(ep[0]-sp[0]>=-1)) && ((ep[1]-sp[1]<=1)&&(ep[1]-sp[1]>=-1)))
                         return true;
                 else  return false;
             }
-            else if(figure==figures[1])
+            else if(*figure==figures[1])
             {   
                 if((ep[0]==sp[0]) && (ep[1]-sp[1]!=0))
                         return true;
@@ -62,7 +77,7 @@ class figure:public Figure
                     return true;
                 else  return false;
             }
-            else if(figure==figures[2])
+            else if(*figure==figures[2])
             {
                 if(((ep[0]-sp[0]==2)||(ep[0]-sp[0]==-2))&&((ep[1]-sp[1]==1)||(ep[1]-sp[1]==-1)))
                     return true;
@@ -70,13 +85,13 @@ class figure:public Figure
                     return true;
                 else return false;
             }
-            else if(figure==figures[3])
+            else if(*figure==figures[3])
             {
                 if(abs(ep[0]-sp[0])==abs(ep[1]-sp[1]))
                     return true;
                 else return false;
             }
-            else if(figure==figures[4])
+            else if(*figure==figures[4])
             {
                 if((ep[0]==sp[0]) && (ep[1]-sp[1]!=0))
                     return true;
@@ -92,14 +107,36 @@ class figure:public Figure
 
 };
 
+class ChessChecker{
+    public:
+        ChessChecker(Parser p,figure fi):par(p)
+	{
+		capros=fi.move(par.Get_Fi(),par.Get_sp(),par.Get_ep());
+	}
+	friend ostream& operator <<(ostream& os,const ChessChecker& CC)
+        {
+            if(CC.capros)
+            os<<"OK";
+            else os<<"ERROR";
+            return os;
+        }
+    private:
+        bool capros;
+	Parser par;
+	figure fi;
+};
+
+
+
 int main()
 {
-    
-    ChessChecker CC;
-    
-    string s;
-    cin>>CC.par;
-    //CC.capros=move(p.figure,p.start_pos,p.end_pos);
-    cout<<CC<<endl;
+    Parser P;
+    figure fi;
+    do{
+	cin>>P;
+	ChessChecker CC(P,fi);
+        cout<<CC<<endl;
+       }
+    while(strcmp(P.Get_Fi(),"exit"));
 }
 
